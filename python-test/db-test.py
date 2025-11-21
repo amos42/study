8000334d1d67a208efaa1e507715e7cb8a49e7c7
Ju, Gyeongmin <46707197+amos42@users.noreply.github.com>
2025-11-21 14:17:45 +0900

    Update db-test.py

--- python-test/db-test.py
+++ python-test/db-test.py
@@ -2,30 +2,28 @@ from sqlalchemy import ARRAY, Sequence, Table, create_engine, Column, Integer, S
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import Session
 from sqlalchemy.schema import CreateTable
-
-
+ 
+ 
 Base = declarative_base()
-
-# 사용자와 게시글 간의 관계를 설정합니다.
-#@as_declarative
+ 
 class MyTest(Base):
     __tablename__ = 'test'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
+ 
+    id = Column(Integer, primary_key=True, autoincrement=True)  # postgresql의 경우엔 SERIAL 타입으로 생성됨
     # id = Column(Integer, Sequence('mytest_id_seq'), primary_key=True, index=True)
     name = Column(String)
     password = Column(String)
     tags = Column(ARRAY(String))
-
-
+ 
+ 
 # 데이터베이스 엔진을 생성합니다.
 # engine = create_engine('postgresql://test:password@localhost:5432/test_db')
 # engine = create_engine('sqlite:///:memory:')
 engine = create_engine('duckdb:///:memory:')
-
+ 
 # 테이블을 생성합니다.
 # Base.metadata.create_all(engine)
-
+ 
 cloned_table = Table(
     MyTest.__tablename__,
     MetaData(),  # 새로운 메타데이터로 충돌 방지
@@ -35,17 +33,17 @@ cloned_table = Table(
         for col in MyTest.__table__.columns
     ]
 )
-
+ 
 print(CreateTable(cloned_table))
-
+ 
 table = cloned_table
 # table = MyTest.__table__
 table.drop(bind=engine, checkfirst=True)
 # seq = Sequence('mytest_id_seq')
 # seq.create(bind=engine)  # 시퀀스 먼저 생성
 table.create(bind=engine)
-
-
+ 
+ 
 # 세션을 생성합니다.
 with Session(engine) as session:
     # # 사용자 데이터를 삽입합니다.
@@ -54,7 +52,7 @@ with Session(engine) as session:
     user = MyTest(name="홍길동2", password="password")
     session.add(user)
     session.commit()
-
+ 
     # 사용자 데이터를 조회합니다.
     users = session.query(MyTest).all()
     for user in users:
